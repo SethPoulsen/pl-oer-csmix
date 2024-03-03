276fe2053ea7a72ffcdbc3a63b07031ea4666657
Mariana Teixeira Silva <mfsilva@illinois.edu>
2024-03-03 17:50:47 +0000

    copy question cache/lru_data_1_mod (from course CS 233) to cache/lru_data_1_mod

---
+++ questions/cache/lru_data_1_mod/server.py
@@ -0,0 +1,10 @@
+from cache_tables import generate_cache
+from cache_tables import parse
+
+def generate(data):
+
+  # generate_cache(data, num_ways, index_bits, num_addr)
+  generate_cache(data, 2, 2, 1)
+
+
+  return data
